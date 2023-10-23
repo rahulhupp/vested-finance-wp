e3f8cdcfe6083e5bc45761e5cce729dbb37f6a1d
rahulhupp <rahul.js@hupp.in>
2023-10-23 18:06:27 +0530

    Add custom function

--- assets/js/slick-slider.js
+++ assets/js/slick-slider.js
@@ -1,48 +1,52 @@
-$("#portfolioSlider").slick({
-	infinite: true,
-	arrows: false,
-	dots: false,
-	autoplay: true,
-	autoplaySpeed: 6000,
-	speed: 600,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	vertical: true,
-	verticalSwiping: true,
-	responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				asNavFor: "#portfolioSliderNav",
-				vertical: false,
-				verticalSwiping: false,
-			},
-		},
-	],
-});
+jQuery(document).ready(function ($) {
 
-$("#portfolioSliderNav").slick({
-	infinite: true,
-	arrows: false,
-	dots: false,
-	autoplay: true,
-	autoplaySpeed: 6000,
-	speed: 600,
-	slidesToShow: 3,
-	slidesToScroll: 1,
-	vertical: true,
-	responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				asNavFor: "portfolioSlider",
-				vertical: false,
-				dots: true,
+	$("#portfolioSlider").slick({
+		infinite: true,
+		arrows: false,
+		dots: false,
+		autoplay: true,
+		autoplaySpeed: 6000,
+		speed: 600,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		vertical: true,
+		verticalSwiping: true,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					asNavFor: "#portfolioSliderNav",
+					vertical: false,
+					verticalSwiping: false,
+				},
 			},
-		},
-	],
+		],
+	});
+
+	$("#portfolioSliderNav").slick({
+		infinite: true,
+		arrows: false,
+		dots: false,
+		autoplay: true,
+		autoplaySpeed: 6000,
+		speed: 600,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		vertical: true,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					asNavFor: "portfolioSlider",
+					vertical: false,
+					dots: true,
+				},
+			},
+		],
+	});
+
 });
