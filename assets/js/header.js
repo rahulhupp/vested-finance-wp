fe7b5b6834d7727de9ec8bd2b0aeb7365e6a04b9
rahulhupp <rahul.js@hupp.in>
2023-10-24 18:25:36 +0530

    Add linkedin slider

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,7 +2,4 @@ jQuery(document).ready(function () {
 	jQuery("header .inner-header .site-primary-header-wrap .logo-menu .humburger").click(function () {
 		jQuery("body").toggleClass("menu-open");
 	});
-	jQuery("header .inner-header .site-primary-header-wrap .logo-menu .menu-overlay").click(function () {
-		jQuery("body").removeClass("menu-open");
-	});
 });
