943f1ae0c182edb2570fce78f2a0b57441a33132
rahulhupp <rahul.js@hupp.in>
2023-11-02 13:01:48 +0530

    Global page

--- inc/enqueue-style-script.php
+++ inc/enqueue-style-script.php
@@ -1,5 +1,8 @@
 <?php
     function enqueue_custom_assets() {
+
+        wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/fontawesome.min.css', array(), '1.8.1', 'all');
+
         // Enqueue the Slick Carousel CSS files from the CDN
         wp_enqueue_style('slick-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css', array(), '1.8.1', 'all');
         wp_enqueue_style('slick-theme', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css', array(), '1.8.1', 'all');
