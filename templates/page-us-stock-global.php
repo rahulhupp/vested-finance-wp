d66df04282fdd22487786eac5028a65a74351575
rahulhupp <rahul.js@hupp.in>
2023-11-02 12:47:10 +0530

    Global page

--- templates/page-us-stock-global.php
+++ templates/page-us-stock-global.php
@@ -13,7 +13,7 @@ get_header(); ?>
                     <div class="bottom">
                         <div class="buttons">
                             <a class="btn_dark" href="<?php the_field('start_investing_link'); ?>"><?php the_field('start_investing_label'); ?></a>
-                            <a class="btn_light" href="#"><i class="fa fa-play" aria-hidden="true"></i><?php the_field('watch_video_label'); ?></a>
+                            <!-- <a class="btn_light" href="#"><i class="fa fa-play" aria-hidden="true"></i><?php the_field('watch_video_label'); ?></a> -->
                         </div>
                     </div>
                 </div>
@@ -97,145 +97,8 @@ get_header(); ?>
         </section>
     <?php endif; ?>
 
-    <section class="calculator">
-        <div class="container">
-            <h1 class="main_heading"><?php the_field('main_heading'); ?></h1>
-            <p class="sub_heading"><?php the_field('main_sub_heading'); ?></p>
-
-
-            <div class="main_calc_wrap">
-                <div class="calc_col">
-
-                    <div class="calc_form_wrap">
-                        <form action="" class="calc_form">
-
-                            <div class="field_group">
-                                <label for="stockSelector">Select Stock</label>
-                                <select name="stockSelector" id="">
-                                    <option value="">Appl, Inc - AAPL </option>
-                                    <option value="">Alphabet </option>
-                                    <option value="">Meta </option>
-                                    <option value="">Amazon </option>
-                                    <option value="">Netflix </option>
-                                    <option value="">Google </option>
-                                </select>
-                            </div>
-
-                            <div class="field_group">
-                                <label for="invest_val">Enter Investment Amount</label>
-
-                                <div class="inner_field">
-                                    <span class="currency">&#x20B9;</span>
-                                    <input type="text" id="invest_val" value="1,000">
-
-                                    <div class="currency_select">
-                                        <div>
-                                            <input type="radio" name="currency" id="inr_currency" checked>
-                                            <label for="inr_currency">INR</label>
-                                        </div>
-                                        <div>
-                                            <input type="radio" name="currency" id="usd_currency">
-                                            <label for="usd_currency">USD</label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="field_note">
-                                    <span><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
-                                            <path d="M10.5 18C14.6421 18 18 14.6421 18 10.5C18 6.35786 14.6421 3 10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18Z" stroke="#002852" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                            <path d="M10.5 13.5V10.5" stroke="#002852" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                            <path d="M10.5 7.5H10.5075" stroke="#002852" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg> Choosing INR will incorporate the FX rate changes as well</span>
-                                </div>
-                            </div>
-
-
-                            <div class="field_group">
-                                <div class="field_row">
-                                    <div class="field_col">
-                                        <label for="startMonth">Select Stock</label>
-                                        <select name="startMonth" id="">
-                                            <option value="">Jan, 2021</option>
-                                            <option value="">Fab, 2021</option>
-                                            <option value="">Mar, 2021</option>
-                                        </select>
-                                    </div>
-                                    <div class="field_col">
-                                        <label for="startMonth">Select Stock</label>
-                                        <select name="startMonth" id="">
-                                            <option value="">Aug, 2023</option>
-                                            <option value="">Jul, 2023</option>
-                                            <option value="">Jun, 2023</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="submit_btn">
-                                <input type="submit" value="calculate">
-                            </div>
-                        </form>
-                    </div>
-
-                </div>
-                <div class="calc_result_col">
-                    <div class="result_inner_col">
-                        <h3>Return Breakdown</h3>
-                        <div class="result_breakdown_wrap">
-                            <div class="result_graph_col">
-                                <!-- <div class="result_circle_wrap">
-                                    <div class="investment_amount_data"></div>
-                                    <div class="returns_data"></div>
-                                </div> -->
-                                <div class="fd_result" id="fd_results">
-                                    <div class="total_value">
-                                        <p>Total Value</p>
-                                        <h4>$<span id="total_calc_val">5,000</span></h4>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="result_breakdown_info">
-
-                                <div class="breakdown_list">
-                                    <div class="invested_val list">
-                                        <p>Invested Amount</p>
-                                        <h4>$ 1,000</h4>
-                                    </div>
-                                    <div class="est_return list">
-                                        <p>Est. Returns</p>
-                                        <h4>$ 4,000</h4>
-                                    </div>
-                                    <div class="total_val list">
-                                        <p>Total Value</p>
-                                        <h4>$5,000</h4>
-                                    </div>
-                                    <div class="cagr_val list">
-                                        <p>CAGR</p>
-                                        <h4>37.97%</h4>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="investment_cta">
-                        <div class="cta_content_col">
-                            <p><?php the_field('cta_text'); ?></p>
-                        </div>
-                        <div class="cta_btn">
-                            <a href="<?php the_field('cta_button_url'); ?>"><?php the_field('cta_button_text'); ?> <i class="fa fa-chevron-right"></i></a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <p class="calc_desc">
-                <?php the_field('calc_disclaimer'); ?>
-            </p>
-        </div>
-    </section>
-
-
+    <?php get_template_part('template-parts/returns-calculator'); ?>
+    
     <?php if (have_rows('portfolio_slider')) : ?>
         <section class="portfolio_slider_sec">
             <div class="container">
@@ -274,62 +137,7 @@ get_header(); ?>
         </section>
     <?php endif; ?>
 
-    <?php if (have_rows('investors_reviews')) : ?>
-        <section class="investors_sec">
-            <div class="container">
-                <h2 class="section_title"><span><?php the_field('investors_heading'); ?></span></h2>
-                <p class="investor_subtitle"><?php the_field('investors_sub_heading'); ?></p>
-                <div class="investors_slider_wrap">
-                    <div class="investors_slider">
-                        <?php while (have_rows('investors_reviews')) : the_row(); ?>
-                            <div class="single_investor_slide">
-                                <div class="investor_slide_inner">
-                                    <div class="investor_details">
-                                        <div class="investor_info">
-                                            <div class="investor_img">
-                                                <img src="<?php the_sub_field('investor_image') ?>" alt="<?php the_sub_field('investor_name') ?>">
-                                            </div>
-                                            <div class="investor_detail">
-                                                <h4 class="invesetor_name">
-                                                    <?php the_sub_field('investor_name') ?>
-                                                </h4>
-                                                <p class="investor_designation">
-                                                    <?php the_sub_field('investor_designation') ?>
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div class="platform_icon">
-                                            <img src="<?php the_sub_field('review_platform_icon') ?>" alt="Review Platform">
-                                        </div>
-                                    </div>
-                                    <div class="investor_review">
-                                        <?php the_sub_field('investor_review') ?>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php endwhile; ?>
-                    </div>
-                    <div class="investor_slider_nav">
-                        <div class="investor_prev">
-                            <svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M27.1035 10.0566H1.74131M1.74131 10.0566L10.9639 0.833984M1.74131 10.0566L10.9639 19.2792" stroke="#002852" stroke-opacity="0.6" stroke-width="1.7" />
-                            </svg>
-                        </div>
-                        <div class="investor_next">
-                            <svg width="27" height="20" viewBox="0 0 27 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M0.283203 10.0566H25.6454M25.6454 10.0566L16.4228 0.833984M25.6454 10.0566L16.4228 19.2792" stroke="#002852" stroke-opacity="0.6" stroke-width="1.7" />
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="investor_desclaimer">
-                    <?php the_field('investors_disclaimer'); ?>
-                </div>
-            </div>
-
-        </section>
-    <?php endif; ?>
+    <?php get_template_part('template-parts/investors-slider'); ?>
 
     <section class="post-type-list">
         <div class="container">
@@ -338,7 +146,7 @@ get_header(); ?>
                 <div class="head">
                     <div class="left-part">
                         <h3>Under the Spotlight</h3>
-                        <a href="#">View All</a>
+                        <a href="https://wordpress-staging.vestedfinance.com/vestedfinance/in/blog/">View All</a>
 
                     </div>
                     <div class="short-content">
@@ -370,7 +178,7 @@ get_header(); ?>
                 <div class="head">
                     <div class="left-part">
                         <h3>Vested Shorts</h3>
-                        <a href="#">View All</a>
+                        <a href="https://wordpress-staging.vestedfinance.com/vestedfinance/in/blog/">View All</a>
 
                     </div>
                     <div class="short-content">
@@ -402,7 +210,7 @@ get_header(); ?>
                 <div class="head">
                     <div class="left-part">
                         <h3>Blogs</h3>
-                        <a href="#">View All</a>
+                        <a href="https://wordpress-staging.vestedfinance.com/vestedfinance/in/blog/">View All</a>
 
                     </div>
                     <div class="short-content">
