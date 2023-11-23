13deb71848b65d8659fafe6f768f2e7358409cfd
rahulhupp <rahul.js@hupp.in>
2023-11-23 12:16:31 +0530

    added social media on vested academy page

--- templates/page-vested-academy.php
+++ templates/page-vested-academy.php
@@ -284,28 +284,28 @@ get_header(); ?>
         });
 
 
-        const BlogData = {
-            title: '<?php the_title(); ?>',
-            url: '<?php the_permalink(); ?>',
+        const drakeProfileData = {
+            title: 'Drake Doppelganger',
+            text: 'üí°Share like a pro from your web application',
+            url: 'https://kvkirthy.github.io/web-share-sample'
         }
-
-        const btn = document.querySelector('.sharing-icon');
-
-        // Share must be triggered by "user activation"
-        btn.addEventListener('click', async () => {
-            try {
-                if (navigator.canShare &&
-                    typeof navigator.canShare === 'function' &&
-                    navigator.canShare(BlogData)) {
-                    let result = await navigator.share(BlogData);
-                    document.getElementById("status").innerText = result || '';
-                } else {
-                    document.getElementById("status").innerText = "Sharing selected data not supported.";
-                }
-            } catch (err) {
-                document.getElementById("status").innerText = "Share not complete";
-            }
-        });
+   
+     const btn = document.querySelector('.sharing-icon');   
+     // Share must be triggered by "user activation"
+     btn.addEventListener('click', async () => {
+       try {
+         if(navigator.canShare 
+             && typeof navigator.canShare === 'function' 
+             && navigator.canShare(drakeProfileData)){
+           let result = await navigator.share(drakeProfileData);
+           document.getElementById("status").innerText = result || '';
+         } else {
+           document.getElementById("status").innerText = "Sharing selected data not supported.";
+         }
+       } catch(err) {
+         document.getElementById("status").innerText = "Share not complete";
+       }
+     });
     </script>
 </div>
 <?php get_footer(); ?>
