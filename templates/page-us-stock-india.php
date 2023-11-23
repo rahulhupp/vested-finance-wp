ec3686ffe0a003bf84c2117b9e5d1e00243f9bf3
rahulhupp <rahul.js@hupp.in>
2023-11-23 19:23:40 +0530

    added links

--- templates/page-us-stock-india.php
+++ templates/page-us-stock-india.php
@@ -189,7 +189,7 @@ get_header(); ?>
                 <div class="head">
                     <div class="left-part">
                         <h3>Under the Spotlight</h3>
-                        <a href="https://wordpress-staging.vestedfinance.com/vestedfinance/in/blog/">View All</a>
+                        <a href="http://wordpress-testing.vestedfinance.com/blog/">View All</a>
 
                     </div>
                     <div class="short-content">
@@ -221,7 +221,7 @@ get_header(); ?>
                 <div class="head">
                     <div class="left-part">
                         <h3>Vested Shorts</h3>
-                        <a href="https://wordpress-staging.vestedfinance.com/vestedfinance/in/blog/">View All</a>
+                        <a href="http://wordpress-testing.vestedfinance.com/blog/">View All</a>
 
                     </div>
                     <div class="short-content">
@@ -253,7 +253,7 @@ get_header(); ?>
                 <div class="head">
                     <div class="left-part">
                         <h3>Blogs</h3>
-                        <a href="https://wordpress-staging.vestedfinance.com/vestedfinance/in/blog/">View All</a>
+                        <a href="http://wordpress-testing.vestedfinance.com/blog/">View All</a>
 
                     </div>
                     <div class="short-content">
