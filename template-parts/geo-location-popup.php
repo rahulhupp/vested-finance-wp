d26d4410d2c6cc97d5bc8b58acfb2787090f5866
rahulhupp <rahul.js@hupp.in>
2023-11-27 16:40:27 +0530

    detect ip address

--- template-parts/geo-location-popup.php
+++ template-parts/geo-location-popup.php
@@ -1,4 +1,4 @@
-<div class="geolocation_banner">
+<div class="geolocation_banner global">
     <div class="left">
         <div class="close">
             <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/close-icon.png">
@@ -9,5 +9,18 @@
     </div>
     <div class="right">
         <a href="<?php echo home_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/india.png">India</a>
+    </div>
+</div>
+<div class="geolocation_banner india">
+    <div class="left">
+        <div class="close">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/close-icon.png">
+        </div>
+        <div class="content">
+            <p>You're on our India website. Visit the Global website to explore our Global-specific products.</p>
+        </div>
+    </div>
+    <div class="right">
+        <a href="<?php echo home_url('in'); ?>"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/global.png">Global</a>
     </div>
 </div>
