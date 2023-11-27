f929077ed8d471c4a14fc6d972c2ca6cb17485ac
rahulhupp <rahul.js@hupp.in>
2023-11-27 16:00:49 +0530

    detect ip address

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,4 +2,7 @@ jQuery(document).ready(function () {
 	jQuery("header .inner-header .site-primary-header-wrap .logo-menu .humburger").click(function () {
 		jQuery("body").toggleClass("menu-open");
 	});
+	jQuery(".geolocation_banner .left .close").click(function () {
+		jQuery(".geolocation_banner").css("display", "none");
+	});
 });
