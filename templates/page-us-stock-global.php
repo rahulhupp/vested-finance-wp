b63fcee22fb6fcc8282958ae5963d818dadec46d
rahulhupp <rahul.js@hupp.in>
2023-11-30 17:58:13 +0530

    FAQ chages

--- templates/page-us-stock-global.php
+++ templates/page-us-stock-global.php
@@ -288,4 +288,25 @@ get_header(); ?>
         </section>
     <?php endif; ?>
 </div>
+<?php if (have_rows('faq_list_global')) : ?>
+<script type="application/ld+json">
+{
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    "mainEntity": [
+        <?php while (have_rows('faq_list_global')) : the_row(); ?>
+            {
+                "@type": "Question",
+                "name": "<?php the_sub_field('faq_question_global') ?>",
+                "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "<?php the_sub_field('faq_answer_global') ?>"
+                }
+            },
+        <?php endwhile; ?>
+    ]
+
+}
+</script>
+<?php endif; ?>
 <?php get_footer(); ?>
