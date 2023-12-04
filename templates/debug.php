72fc07cc0129f82bdf2a8a220d6eb8ace7d0e513
rahulhupp <rahul.js@hupp.in>
2023-12-04 18:00:59 +0530

    Geo Location

--- templates/debug.php
+++ templates/debug.php
@@ -2,14 +2,36 @@
 /*
 Template name: Page - Debug
 */
-get_header(); ?>
-<?php $mycountry  = do_shortcode('[geoip_detect2 property="country"]');  ?>
+get_header(); 
+die('asdasd')
+?>
 <?php
-    echo $mycountry;
-    if ($mycountry === 'India') {
-        echo "India";
-    } else {
-        echo "Global";
-    }
+
+    function get_client_ip() {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+           $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+           $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+           $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+           $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+           $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+           $ipaddress = getenv('REMOTE_ADDR');
+        else
+           $ipaddress = 'UNKNOWN';
+   
+        return $ipaddress;
+   }
+
+    $myipd = get_client_ip(); 
+    $userInfoo = geoip_detect2_get_info_from_ip($myipd, NULL);
+    echo $userInfoo->country->isoCode;
+
+
+
 ?>
 <?php get_footer(); ?>
