fe1677477b10bf3ceefd1415305ef05d481d1c0e
rahulhupp <rahul.js@hupp.in>
2023-12-04 16:01:49 +0530

    Geo Location

--- templates/debug.php
+++ templates/debug.php
@@ -1,1 +1,7 @@
+<?php
+/*
+Template name: Page - Debug
+*/
+get_header(); ?>
 <?php echo do_shortcode('[geoip_detect2 property="country.name" default="(country could not be detected)" lang="en"]')  ?>
+<?php get_footer(); ?>
