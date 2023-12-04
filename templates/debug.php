70d35c8746e4736d27691c804c994b5de98c3375
rahulhupp <rahul.js@hupp.in>
2023-12-04 16:31:20 +0530

    Geo Location

--- templates/debug.php
+++ templates/debug.php
@@ -5,6 +5,9 @@ Template name: Page - Debug
 get_header(); ?>
 <?php $mycountry  = do_shortcode('[geoip_detect2 property="country"]');  ?>
 <?php
+    $userInfo = geoip_detect2_get_info_from_current_ip();
+    echo $userInfo->country->isoCode;
+    echo "<br/> <br/> <br/> <br/>";
     echo $mycountry;
     if ($mycountry === 'India') {
         echo "India";
