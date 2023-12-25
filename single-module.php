62d27033c6ab439fba054ba941105e0455ee8776
rahulhupp <rahul.js@hupp.in>
2023-12-25 11:33:14 +0530

    single modual css page  chnages

--- single-module.php
+++ single-module.php
@@ -302,7 +302,7 @@ while (have_posts()) :
 
         window.addEventListener('scroll', () => {
             const scrollTop = window.scrollY;
-            if (scrollTop > 20) {
+            if (scrollTop > 0) {
                 triggerElement.classList.add('scrolledd');
             } else {
                 triggerElement.classList.remove('scrolledd');
