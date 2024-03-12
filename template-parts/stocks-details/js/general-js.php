074ab4027e0f230beafed97e5606d2e0dcb46a98
rahulhupp <rahul.js@hupp.in>
2024-03-12 11:45:09 +0530

    Reduce JavaScript execution time

--- template-parts/stocks-details/js/general-js.php
+++ template-parts/stocks-details/js/general-js.php
@@ -1,5 +1,4 @@
 <script defer>
-    let start = performance.now();
     const anchorLinks = document.querySelectorAll('a[href^="#"]');
     anchorLinks.forEach(function(link) {
         link.addEventListener('click', function(e) {
@@ -15,8 +14,6 @@
             }
         });
     });
-    let timeTaken = performance.now() - start;
-    console.log("Total time taken : " + timeTaken + " milliseconds");
     
     const faqItems = document.querySelectorAll('.faq_item');
     faqItems.forEach(item => {
@@ -227,60 +224,67 @@
 
     document.addEventListener('DOMContentLoaded', function() {
         setTimeout(function() {
-            document.querySelectorAll('.trend_chart').forEach(function(cell) {
-                var trendData = JSON.parse(cell.textContent);
-                if (trendData.length > 0) {
-                    cell.textContent = '';
-                    var chartContainer = document.createElement('div');
-                    cell.appendChild(chartContainer);
-                    var canvas = document.createElement('canvas');
-                    canvas.width = 400;
-                    canvas.height = 400;
-                    chartContainer.appendChild(canvas);
-                    var dates = [];
-                    var values = [];
-                    trendData.forEach(function(trend) {
-                        dates.push(trend.date);
-                        values.push(trend.value);
-                    });
-                    // Create a chart
-                    var ctx = canvas.getContext('2d');
-                    var myChart = new Chart(ctx, {
-                        type: 'bar',
-                        data: {
-                            labels: dates,
-                            datasets: [{
-                                label: 'Values',
-                                data: values,
-                                borderWidth: 1,
-                                backgroundColor: values.map(item => item < 0 ? "#b92406" : "#008a5a"),
-                                borderColor: values.map(item => item < 0 ? "#b92406" : "#008a5a"),
-                            }]
-                        },
-                        options: {
-                            scales: {
-                                x: {
-                                    display: false, // Hide x-axis labels
-                                },
-                                y: {
-                                    display: false, // Hide y-axis labels
-                                    beginAtZero: true
-                                }
-                            },
-                            plugins: {
-                                legend: {
-                                    display: false // Hide legends
-                                },
-                                tooltip: {
-                                    enabled: false // Hide tooltips
-                                }
-                            }
-                        },
-                    });
-                }
-            });
+            let start = performance.now();
+            addFinancialChart();
+            let timeTaken = performance.now() - start;
+            console.log("Total time taken : " + timeTaken + " milliseconds");
         }, 500); 
     });
+
+    function addFinancialChart() {
+        document.querySelectorAll('.trend_chart').forEach(function(cell) {
+            var trendData = JSON.parse(cell.textContent);
+            if (trendData.length > 0) {
+                cell.textContent = '';
+                var chartContainer = document.createElement('div');
+                cell.appendChild(chartContainer);
+                var canvas = document.createElement('canvas');
+                canvas.width = 400;
+                canvas.height = 400;
+                chartContainer.appendChild(canvas);
+                var dates = [];
+                var values = [];
+                trendData.forEach(function(trend) {
+                    dates.push(trend.date);
+                    values.push(trend.value);
+                });
+                // Create a chart
+                var ctx = canvas.getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: dates,
+                        datasets: [{
+                            label: 'Values',
+                            data: values,
+                            borderWidth: 1,
+                            backgroundColor: values.map(item => item < 0 ? "#b92406" : "#008a5a"),
+                            borderColor: values.map(item => item < 0 ? "#b92406" : "#008a5a"),
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            x: {
+                                display: false, // Hide x-axis labels
+                            },
+                            y: {
+                                display: false, // Hide y-axis labels
+                                beginAtZero: true
+                            }
+                        },
+                        plugins: {
+                            legend: {
+                                display: false // Hide legends
+                            },
+                            tooltip: {
+                                enabled: false // Hide tooltips
+                            }
+                        }
+                    },
+                });
+            }
+        });
+    }
 
 
     const valueTypeSelect = document.getElementById("value_type_select");
