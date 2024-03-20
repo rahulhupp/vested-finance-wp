b286babc15fb68ded319de8b63b07b535ad4beb2
rahulhupp <rahul.js@hupp.in>
2024-03-20 18:33:24 +0530

    ETFs review points

--- template-parts/stocks-details/largest-holdings.php
+++ template-parts/stocks-details/largest-holdings.php
@@ -1,4 +1,12 @@
-<?php $largest_holdings_data = $args['largest_holdings_data']; ?>
+<?php 
+    $largest_holdings_data = $args['largest_holdings_data'];
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'stocks_list';
+    $wpdb->query("SET SESSION group_concat_max_len = 100000");
+    $symbols_concatenated = $wpdb->get_var("SELECT GROUP_CONCAT(symbol SEPARATOR ',') AS symbols FROM $table_name");
+    $wpdb->query("SET SESSION group_concat_max_len = @@group_concat_max_len");
+    $symbols = explode(',', $symbols_concatenated);
+?>
 <?php if ($largest_holdings_data) { ?>
     <div id="largest_holdings_tab" class="tab_content">
         <div class="stock_details_box">
@@ -16,26 +24,28 @@
                                 </tr>
                                 <?php
                                     foreach ($largest_holdings_data as $holdings) {
+                                        $holdingTicker = preg_replace('/[^a-zA-Z0-9]/', '.', $holdings['ticker']);
                                         ?>
                                             <tr>
                                                 <td>
                                                     <div class="holdings_stock_name">
-                                                        <img src="https://d13dxy5z8now6z.cloudfront.net/symbol/<?php echo $holdings['ticker']; ?>.png" alt="<?php echo $holdings['ticker']; ?>-img" class="holdings_image" />
+                                                        <img src="https://d13dxy5z8now6z.cloudfront.net/symbol/<?php echo $holdingTicker; ?>.png" alt="<?php echo $holdingTicker; ?>-img" class="holdings_image" />
                                                         <span><?php echo $holdings['name']; ?></span>
                                                     </div>
                                                 </td>
                                                 <td>
                                                     <?php
-                                                        $symbol = strtolower($holdings['ticker']);
+                                                        $symbol = strtolower($holdingTicker);
                                                         $name = strtolower($holdings['name']);
                                                         $name = str_replace([' ', ','], '-', $name);
                                                         $name = preg_replace('/[^a-zA-Z0-9\-]/', '', $name);
                                                         $name = preg_replace('/-+/', '-', $name);
                                                         $name = trim($name, '-');
                                                     ?>
-                                                    <a href="<?php echo home_url(); ?>/us-stocks/<?php echo $symbol; ?>/<?php echo $name ?>-share-price/">
-                                                        <?php echo $holdings['ticker']; ?>
+                                                    <a <?php if (in_array($holdingTicker, $symbols)) { ?> href="<?php echo home_url(); ?>/us-stocks/<?php echo $symbol; ?>/<?php echo $name ?>-share-price/" <?php } ?> >
+                                                        <?php echo $holdingTicker; ?>
                                                     </a>
+                                                    
                                                 </td>
                                                 <td class="holdings_stock_weight_td">
                                                     <div class="holdings_stock_weight">
