dcb5d93d7ebba0619e45fb58a080deceac72e2ba
rahulhupp <rahul.js@hupp.in>
2024-08-02 18:22:04 +0530

    Add new note to home page

--- templates/page-home-page.php
+++ templates/page-home-page.php
@@ -407,10 +407,9 @@ $image = get_field('easy_access_image');
             if (globalBanner) {
                 globalBanner.style.display = "flex"; 
                 if (data.country === "IN") {
-                    globalBanner.style.display = "none"; 
-                    // globalBanner.innerHTML = "<div class='content'><img src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/alert-circle-icon.svg' /><p>Vested will never ask you to make any payments through WhatsApp for account creation or other services. Always ensure you are using our official website, mobile app, or referral links from trusted family or friends to create your Vested account. <a href='https://vestedfinance.com/blog/vested-updates/urgent-fraud-alert-beware-of-whatsapp-scams/' target='_blank' class='learn_more_btn'>Learn more</a></p></div>";
+                    globalBanner.innerHTML = "<div class='content'><p>Announcing our latest partnership with HDFC Securities powering their Global Investing 2.0 offering. <a href='https://bfsi.economictimes.indiatimes.com/news/financial-services/hdfc-securities-partners-with-vested-finance-to-offer-access-to-global-investing-for-indians-and-nris/112221333' target='_blank' class='learn_more_btn'>Read Press Release</a></p></div>";
                     // globalBanner.classList.add('warning_banner');
-                    // console.log('show geolocation_banner');
+                    console.log('show geolocation_banner');
                 } else {
                     globalBanner.innerHTML = "<div class='content'><p>You're on our India website. Visit the Global website to explore our Global products.</p></div><a href='<?php home_url() ?>'><img src='<?php echo get_stylesheet_directory_uri(); ?>/assets/images/global.png'>Global</a>";
                 }
