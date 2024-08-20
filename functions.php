521c3a1c127c745eb2876ccc0f4344b90a42873a
rahulhupp <rahul.js@hupp.in>
2024-08-20 14:24:47 +0530

    Bond detail funcstions added in functions file

--- functions.php
+++ functions.php
@@ -22,6 +22,7 @@ require_once get_stylesheet_directory() . '/inc/store-token.php';
 require_once get_stylesheet_directory() . '/inc/stocks-details-fucntions.php';
 require_once get_stylesheet_directory() . '/template-parts/stocks-details/fetch-stocks-api-data.php';
 require_once get_stylesheet_directory() . '/template-parts/fetch-inr-bonds-api-data.php';
+require_once get_stylesheet_directory() . '/inc/bond-details-functions.php';
 
 function add_custom_js_to_pages() {
     if (is_page()) { // You can specify conditions if needed
