7868d656442628ea5fddc07ba65b20afd4381d46
rahulhupp <rahul.js@hupp.in>
2024-12-26 17:26:04 +0530

    Update image width and height in zebpay page

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const images = document.querySelectorAll("img");
   images.forEach((img) => {
     if (!img.hasAttribute("width") && !img.hasAttribute("height")) {
-      img.setAttribute("width", img.naturalWidth || 1);
-      img.setAttribute("height", img.naturalHeight || 1);
+      img.setAttribute("width", img.naturalWidth || 'auto');
+      img.setAttribute("height", img.naturalHeight || 'auto');
     }
     if (!img.hasAttribute("loading")) {
       img.setAttribute("loading", "lazy");
