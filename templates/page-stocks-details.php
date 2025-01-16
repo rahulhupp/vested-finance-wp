1c87cb903e7fbfdfe26b97f70e0882b7647b2f80
rahulhupp <rahul.js@hupp.in>
2025-01-16 14:50:32 +0530

    Debugging issue in sector breakdown template part for eft stocks

--- templates/page-stocks-details.php
+++ templates/page-stocks-details.php
@@ -290,6 +290,12 @@ get_header();
                             get_template_part('template-parts/stocks-details/largest-holdings', null, array('largest_holdings_data' => $largest_holdings_data));
                         }
                         ?>
+                        
+                        <?php
+                        if ($sector_breakdowns_data) {
+                            get_template_part('template-parts/stocks-details/sector-breakdown', null, array('sector_breakdowns_data' => $sector_breakdowns_data));
+                        }
+                        ?>
 
                     </div>
                 </div>
