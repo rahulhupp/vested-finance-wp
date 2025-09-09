b404e64b4273c5cef965b0a7961ebf5ad0e0e885
rahulhupp <rahul.js@hupp.in>
2025-09-09 16:48:02 +0530

    Add exclusive-benefits

--- inc/enqueue-style-script.php
+++ inc/enqueue-style-script.php
@@ -134,6 +134,12 @@
             wp_enqueue_style('home-new-css', get_stylesheet_directory_uri() . '/assets/css/templates/css-home-new.css', false, '', '');
             wp_enqueue_script('home-new-js', get_stylesheet_directory_uri() . '/assets/js/templates/js-home-new.js');
         }
+        if (is_singular('exclusive-benefits')) {
+            wp_enqueue_script('gsap-js', 'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js');
+            wp_enqueue_script('gsap-scrolltrigger-js', 'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollTrigger.min.js');
+            wp_enqueue_style('exclusive-benefits-style', get_stylesheet_directory_uri() . '/assets/css/css-exclusive-benefits.css', false, '', '');
+            wp_enqueue_script('exclusive-benefits-js', get_stylesheet_directory_uri() . '/assets/js/js-exclusive-benefits.js');
+        }
     }
 
     add_action('wp_enqueue_scripts', 'enqueue_custom_assets');
