d72fac48b4d66728eeed7ea8cb7e49ff713284d3
rahulhupp <rahul.js@hupp.in>
2025-12-15 11:01:30 +0530

    Update PDF file

--- templates/page-global-investing-report.php
+++ templates/page-global-investing-report.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="robots" content="noindex, nofollow">
+	<!-- <meta name="robots" content="noindex, nofollow"> -->
 	<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
 	<?php wp_head(); ?>
 	<style>
